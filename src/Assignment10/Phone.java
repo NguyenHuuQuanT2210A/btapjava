9b082b802e69313bbc57ea3c38b6d44386841c7c
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-05-11 14:32:42 +0700

    Delete src/Assignment10 directory

--- src/Assignment10/Phone.java
+++
@@ -1,9 +0,0 @@
-package Assignment10;
-
-public abstract class Phone {
-    public abstract void insertPhone(String name, String phone);
-    public abstract void removePhone(String name);
-    public abstract void updatePhone(String name,String newphone);
-    public abstract PhoneNumber searchPhone(String name);
-    public abstract void sort();
-}
